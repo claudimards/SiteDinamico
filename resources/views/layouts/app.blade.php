ad03f615b09f9444296b0607c4a4212ee97471eb
claudimar <claudimards@gmail.com>
2020-07-25 00:13:01 -0300

    Adicionado jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="stylesheet" type="text/css" href="{{ asset('lib/materialize/dist/css/materialize.css') }}">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -76,5 +69,7 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('lib/jquery/dist/jquery.js') }}"></script>
+    <script src="{{ asset('lib/materialize/dist/js/materialize.js') }}"></script>
 </body>
 </html>
