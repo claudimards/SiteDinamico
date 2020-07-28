ee37cafc3545ef67c081977798c9ba4ebff7691a
claudimar <claudimards@gmail.com>
2020-07-28 14:56:05 -0300

    Adicionado listagem de imóveis

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,12 +4,6 @@
 
 @include('layouts._site._slides')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="panel-body">
-                Home
-            </div>
-        </div>
-    </div>
+    @include('layouts._site._lista_imoveis')
 </div>
 @endsection
