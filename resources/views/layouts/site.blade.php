628d39b8d77a02492d7cb069d412ee2043bc89e0
claudimar <claudimards@gmail.com>
2020-07-28 01:38:55 -0300

    HTML refatorado e implementado botões de menu do site

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -21,14 +21,18 @@
         <nav>
             <div class="nav-wrapper blue">
                 <div class="container">
-                    <a href="#!" class="brand-logo">Logo</a>
+                    <a href="#!" class="brand-logo">SiteDinâmico</a>
                     <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
                     </ul>
                     
                     <ul class="sidenav" id="mobile-demo">
                         <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
                     </ul>
                 </div>
             </div>
