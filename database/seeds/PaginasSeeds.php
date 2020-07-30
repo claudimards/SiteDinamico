8ba34ca7d526817382940d70b6a3a389d41f293b
claudimar <claudimards@gmail.com>
2020-07-30 17:03:11 -0300

    Definido a rota, o controller e a view da página Sobre

--- database/seeds/PaginasSeeds.php
+++ database/seeds/PaginasSeeds.php
@@ -20,10 +20,19 @@ class PaginasSeeds extends Seeder
             $paginaSobre = new Pagina();
         }
 
-        $paginaSobre->titulo = "A empresa";
+        $paginaSobre->titulo = "Título da empresa";
         $paginaSobre->descricao = "Descrição breve sobre a empresa";
         $paginaSobre->texto = "Texto sobre a empresa";
-        $paginaSobre->imagem = "site/img/modelo-img-home.bmp";
+        $paginaSobre->imagem = "img/modelo-img-home.bmp";
+        $paginaSobre->mapa = '<iframe 
+                src="https://www.google.com/maps/embed?
+                pb=!1m18!1m12!1m3!1d3673.229275544551!2d-47.12888418558153!3d-22.
+                978594984975327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x
+                94c8c9f1858984ad%3A0x94511e1cd69fe975!2sDic%20V%20
+                (Conjunto%20Hab.%20Chico%20Mendes)%2C%20Campinas%20-%20SP%2C%2013054-500
+                !5e0!3m2!1spt-BR!2sbr!4v1596138959120!5m2!1spt-BR!2sbr" width="450" height="300" 
+                frameborder="0" style="border:0;" allowfullscreen="false" aria-hidden="false" tabindex="0">
+            </iframe>';
         $paginaSobre->tipo = "sobre";
         $paginaSobre->save();
     }
