56d7e08bf47ea3ab26258a80a99fe8072e70470f
claudimar <claudimards@gmail.com>
2020-08-25 17:15:15 -0300

    Adicionando imagens à galeria

--- app/Http/Controllers/Admin/GaleriaController.php
+++ app/Http/Controllers/Admin/GaleriaController.php
@@ -6,6 +6,8 @@ use App\Galeria;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Imovel;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Session;
 
 
 class GaleriaController extends Controller
@@ -17,56 +19,50 @@ class GaleriaController extends Controller
         return view('admin.galerias.index', compact('registros', 'imovel'));
     }
 
-    public function adicionar()
+    public function adicionar($id)
     {
-        $tipos = Tipo::all();
-        $cidades = Cidade::all();
+        $imovel = Imovel::find($id);
 
-        return view('admin.imoveis.adicionar', compact('tipos', 'cidades'));
+        return view('admin.galerias.adicionar', compact('imovel'));
     }
 
-    public function salvar(Request $request)
+    public function salvar(Request $request, $id)
     {
+        $imovel = Imovel::find($id);
+
         $dados = $request->all();
-        $registro = new Imovel();
-        $registro->titulo = $dados['titulo'];
-        $registro->descricao = $dados['descricao'];
-        $registro->status = $dados['status'];
-        $registro->endereco = $dados['endereco'];
-        $registro->cep = $dados['cep'];
-        $registro->valor = $dados['valor'];
-        $registro->dormitorios = $dados['dormitorios'];
-        $registro->detalhes = $dados['detalhes'];
-        $registro->visualizacoes = 0;
-        $registro->publicar = $dados['publicar'];
 
-        if(isset($dados['mapa']) && trim($dados['mapa']) != ""){
-            $registro->mapa = trim($dados['mapa']);
+        if($imovel->galeria()->count()){
+            $galeria = $imovel->galeria()->orderBy('ordem', 'desc')->first();
+            $ordemAtual = $galeria->ordem;
         }else{
-            $registro->mapa = null;
+            $ordemAtual = 0;
         }
 
-        $registro->cidade_id = $dados['cidade_id'];
-        $registro->tipo_id = $dados['tipo_id'];
-        $file = $request->file('imagem');
+        if($request->hasFile('imagens')){
+            $arquivos = $request->file('imagens');
+            foreach($arquivos as $imagem){
+                $registro = new Galeria();
 
-        if($file){
-            $rand = rand(11111, 99999);
-            $diretorio = "img/imoveis/".Str::slug($dados['titulo'], '_')."/";
-            $ext = $file->guessClientExtension();
-            $nomeArquivo = "_img_".$rand.".".$ext;
-            $file->move($diretorio, $nomeArquivo);
-            $registro->imagem = $diretorio.'/'.$nomeArquivo;
-        }
-
-        $registro->save();
+                $rand = rand(11111, 99999);
+                $diretorio = "img/imoveis/".Str::slug($imovel->titulo, '_')."/";
+                $ext = $imagem->guessClientExtension();
+                $nomeArquivo = "_img_".$rand.".".$ext;
+                $imagem->move($diretorio, $nomeArquivo);
+                $registro->imovel_id = $imovel->id;
+                $registro->ordem = $ordemAtual + 1;
+                $ordemAtual++;
+                $registro->imagem = $diretorio.'/'.$nomeArquivo;
+                $registro->save();
+            }
+        }     
 
         Session::flash('mensagem', [
             'msg'=>'Registro adicionado com sucesso!',
             'class'=>'green white-text'
         ]);
 
-        return redirect(route('admin.imoveis'));
+        return redirect(route('admin.galerias', $imovel->id));
     }
 
     public function editar($id)
--- resources/views/admin/galerias/adicionar.blade.php
+++ resources/views/admin/galerias/adicionar.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h2 class="center">Adicionar Imóvel</h2>
+        <h2 class="center">Adicionar Imagem</h2>
         <div class="row">
             <nav>
                 <div class="nav-wrapper green">
                     <div class="col s12">
                         <a href="{{ route('admin.principal') }}" class="breadcrumb">Início</a>
                         <a href="{{ route('admin.imoveis') }}" class="breadcrumb">Lista de Imóveis</a>
-                        <a class="breadcrumb">Adicionar Imóvel</a>
+                        <a href="{{ route('admin.galerias', $imovel->id) }}" class="breadcrumb">Galeria de imagens</a>
+                        <a class="breadcrumb">Adicionar Imagem</a>
                     </div>
                 </div>
             </nav>            
         </div>
         <div class="row">
-            <form action="{{ route('admin.imoveis.salvar') }}" method="post"
+            <form action="{{ route('admin.galerias.salvar', $imovel->id) }}" method="post"
                 enctype="multipart/form-data">
                 {{ csrf_field() }}
-                @include('admin.imoveis._form')
+                @include('admin.galerias._form')
                 <button class="btn blue">Adicionar</button>
             </form>
         </div>
