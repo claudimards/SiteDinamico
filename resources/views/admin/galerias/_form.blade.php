fb6f5416e453a7219d51fe8a7b8b67d1840cbe42
claudimar <claudimards@gmail.com>
2020-08-25 16:21:36 -0300

    Listando imagens da galeria

---
+++ resources/views/admin/galerias/_form.blade.php
@@ -0,0 +1,93 @@
+<div class="input-field">
+    <input type="text" name="titulo" class="validate" 
+        value="{{ isset($registro->titulo) ? $registro->titulo : '' }}">
+    <label>Título</label>
+</div>
+<div class="input-field">
+    <input type="text" name="descricao" class="validate" 
+        value="{{ isset($registro->descricao) ? $registro->descricao : '' }}">
+    <label>Descrição</label>
+</div>
+<div class="row">
+    <div class="file-field input-field col m6 s12">
+        <div class="btn">
+            <span>Imagem</span>
+            <input type="file" name="imagem">
+        </div>
+        <div class="file-path-wrapper">
+            <input type="text" class="file-path validate">
+        </div>
+    </div>
+    <div class="col m6 s12">
+    @if(isset($registro->imagem))
+        <img width="120" src="{{ asset($registro->imagem) }}">
+    @endif
+    </div>
+</div>
+<div class="input-field col s12">
+    <select name="status">
+        <option value="aluga" 
+            {{ (isset($registro->status) && $registro->status === 'aluga' ? 'selected' : '') }}>Aluga</option>
+        <option value="vende"
+            {{ (isset($registro->status) && $registro->status === 'vende' ? 'selected' : '') }}>Vende</option>
+    </select>
+    <label for="status">Status</label>
+</div>
+<div class="input-field col s12">
+    <select name="tipo_id">
+        @foreach($tipos as $tipo)
+        <option value="{{ $tipo->id }}" {{ (isset($registro->tipo_id) && $registro->tipo_id == $tipo->id ? 'selected' : '') }}>
+            {{ $tipo->tipo }}
+        </option>
+        @endforeach
+    </select>
+    <label for="tipo_id">Tipo do Imóvel</label>
+</div>
+<div class="input-field">
+    <input type="text" name="endereco" class="validate"
+        value="{{ (isset($registro->endereco) ? $registro->endereco : '') }}">
+    <label for="endereco">Endereço</label>
+</div>
+<div class="input-field">
+    <input type="text" name="cep" class="validate"
+        value="{{ (isset($registro->cep) ? $registro->cep : '') }}">
+    <label for="cep">CEP (Ex: XXXXX-XXX)</label>
+</div>
+<div class="input-field col s12">
+    <select name="cidade_id">
+    @foreach($cidades as $cidade)
+        <option value="{{ $cidade->id }}" {{ (isset($registro->cidade_id) && $registro->cidade_id == $cidade->id ? 'selected' : '') }}>
+            {{ $cidade->nome }}
+        </option>
+    @endforeach
+    </select>
+    <label for="cidade_id">Cidade</label>
+</div>
+<div class="input-field">
+    <input type="text" name="valor" class="validate"
+        value="{{ (isset($registro->valor) ? $registro->valor : '') }}">
+    <label for="valor">Valor (Ex: 234.90)</label>
+</div>
+<div class="input-field">
+    <input type="text" name="dormitorios" class="validate"
+        value="{{ (isset($registro->dormitorios) ? $registro->dormitorios : '') }}">
+    <label for="dormitorios">Dormitórios (Ex: 3)</label>
+</div>
+<div class="input-field">
+    <input type="text" name="detalhes" class="validate"
+        value="{{ (isset($registro->detalhes) ? $registro->detalhes: '') }}">
+    <label for="detalhes">Detalhes</label>
+</div>
+<div class="input-field">
+    <textarea name="mapa" class="materialize-textarea">
+        {{ (isset($registro->mapa) ? $registro->mapa : '') }}
+    </textarea>
+    <label for="mapa">Mapa (Cole o iframe do Google Maps)</label>
+</div>
+<div class="input-field col s12">
+    <select name="publicar">
+        <option value="nao" {{ (isset($registro->publicar) && $registro->publicar === 'nao' ? 'selected' : '') }}>Não</option>
+        <option value="sim" {{ (isset($registro->publicar) && $registro->publicar === 'sim' ? 'selected' : '') }}>Sim</option>
+    </select>
+    <label for="publicar">Publicar?</label>
+</div>
---
+++ resources/views/admin/galerias/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h2 class="center">Galeria de imagens</h2>
+        <div class="row">
+            <nav>
+                <div class="nav-wrapper green">
+                <div class="col s12">
+                    <a href="{{ route('admin.principal') }}" class="breadcrumb">Início</a>
+                    <a href="{{ route('admin.imoveis') }}" class="breadcrumb">Lista de Imóveis</a>
+                    <a class="breadcrumb">Galeria de imagens</a>
+                </div>
+                </div>
+            </nav>            
+        </div>
+        <div class="row">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>TÍTULO</th>
+                        <th>DESCRIÇÃO</th>
+                        <th>IMAGEM</th>
+                        <th>ORDEM</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($registros as $registro)
+                    <tr>
+                        <td>{{ $registro->id }}</td>
+                        <td>{{ $registro->titulo }}</td>
+                        <td>{{ $registro->descricao }}</td>
+                        <td><img width="100" src="{{ asset($registro->imagem) }}"></td>
+                        <td>{{ $registro->ordem }}</td>
+                        <td>
+                            <a class="btn orange" href="{{ route('admin.galerias.editar', $registro->id) }}">Editar</a>
+                            <a class="btn red" href="javascript: if(confirm('Deletar esse registro?')){
+                                window.location.href = '{{ route('admin.galerias.deletar', $registro->id) }}'
+                            }">Deletar</a>
+                            <a></a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="row">
+            <a class="btn blue" href="{{ route('admin.galerias.adicionar', $imovel->id) }}">Adicionar</a>
+        </div>
+    </div>
+@endsection
