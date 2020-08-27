4b8cd108400de37d978a1ca6cc0e2a6d476a9208
claudimar <claudimards@gmail.com>
2020-08-27 12:57:01 -0300

    Finalizado a implementação do CRUD de papéis

--- app/Http/Controllers/Admin/PapelController.php
+++ app/Http/Controllers/Admin/PapelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Papel;
+use Illuminate\Support\Facades\Session;
 
 class PapelController extends Controller
 {
@@ -17,5 +18,55 @@ class PapelController extends Controller
     public function adicionar()
     {
         return view('admin.papel.adicionar');
+    }
+
+    public function salvar(Request $request)
+    {
+        Papel::create($request->all());
+
+        return redirect(route('admin.papel'));
+    }
+
+    public function editar($id)
+    {
+        if(Papel::find($id)->nome == 'admin'){
+            return redirect(route('admin.papel'));
+        }
+
+        $registro = Papel::find($id);
+
+        return view('admin.papel.editar', compact('registro'));
+    }
+
+    public function atualizar(Request $request, $id)
+    {
+        if(Papel::find($id)->nome == 'admin'){
+            return redirect(route('admin.papel'));
+        }
+
+        Papel::find($id)->update($request->all());
+
+        Session::flash('mensagem', [
+            'msg'=>'Registro atualizado com sucesso!',
+            'class'=>'green white-text'
+        ]);
+
+        return redirect(route('admin.papel'));
+    }
+
+    public function deletar($id)
+    {
+        if(Papel::find($id)->nome == 'admin'){
+            return redirect(route('admin.papel'));
+        }
+
+        Papel::find($id)->delete();
+
+        Session::flash('mensagem', [
+            'msg'=>'Registro deletado com sucesso!',
+            'class'=>'red white-text'
+        ]);
+
+        return redirect(route('admin.papel'));
     }
 }
--- resources/views/admin/papel/index.blade.php
+++ resources/views/admin/papel/index.blade.php
@@ -29,11 +29,19 @@
                         <td>{{ $registro->nome }}</td>
                         <td>{{ $registro->descricao }}</td>
                         <td>
+                            @if($registro->nome != 'admin')
                             <a class="btn orange" href="{{ route('admin.papel.editar', $registro->id) }}">Editar</a>
+                            @else
+                            <a class="btn orange disabled">Editar</a>
+                            @endif
+
+                            @if($registro->nome != 'admin')
                             <a class="btn red" href="javascript: if(confirm('Deletar esse registro?')){
                                 window.location.href = '{{ route('admin.papel.deletar', $registro->id) }}'
                             }">Deletar</a>
-                            <a></a>
+                            @else
+                            <a class="btn orange disabled">Deletar</a>
+                            @endif
                         </td>
                     </tr>
                 @endforeach
